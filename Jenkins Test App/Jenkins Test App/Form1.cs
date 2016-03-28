31bee7049f7349cb4b74c7c2a655d100ba5a4950
Praveen Kumar Singh <23148127@mahindra.com>
2016-03-28 19:39:16 +0530

    Added files via upload

---
+++ Jenkins Test App/Jenkins Test App/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jenkins_Test_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnAdd_Click(object sender, EventArgs e)
+        {
+            int a;
+            int b;
+            if (!int.TryParse(textBox1.Text, out a))
+            {
+                MessageBox.Show("I need just a number in the first box!");
+                return;
+            }
+            if (!int.TryParse(textBox2.Text, out b))
+            {
+                MessageBox.Show("I need just a number in the second box!");
+                return;
+            }
+            label2.Text = (a + b).ToString();
+        }
+    }
+}
---
+++ Jenkins Test App/Jenkins Test AppTests/Form1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jenkins_Test_App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenkins_Test_App.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void btnAdd_ClickTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
